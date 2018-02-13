6d990da2765f3f03f97afb30e5e5bd2def8d7b1d
asieduernest <asieduernest@yahoo.co.uk>
2018-02-13 14:41:39 -0500

    added DeckOfCards main class

---
+++ src/DeckOfCardsTest.java
@@ -0,0 +1,19 @@
+
+public class DeckOfCardsTest {
+
+	public static void main(String[] args){
+
+		DeckOfCards mydeckofCards = new DeckOfCards();
+
+		mydeckofCards.shuffle();
+
+		for(int i=1; i <= DeckOfCards.NUMBER_OF_CARDS; i++){
+			System.out.printf("%-19s", mydeckofCards.dealOneCard());
+
+			if(i%4 == 0)
+				System.out.println();
+
+		}
+	}
+
+}
